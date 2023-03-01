c9dbf290384e8a73d172f14a6173f1e2d35c3f46
Meetraj <rathod.meetraj.d@gmail.com>
2023-03-01 18:32:52 +0530

    Practice functions, lambda expression, inline functions, null safety, scoping functions(let, run, also, apply, with) in Kotlin

--- KotlinPractice/app/src/main/java/com/example/kotlinpractice/practice.kt
+++ KotlinPractice/app/src/main/java/com/example/kotlinpractice/practice.kt
@@ -1,21 +1,20 @@
 package com.example.kotlinpractice
 
-import kotlin.reflect.typeOf
+import java.util.Scanner
 
-const val constantVal: String = ""
-
-//fun swap(val1: Int, val2: Int) {
-//    val temp = val1
-//    val1 = val2
-//    val2 = temp
-//}
-
+const val constantVal: String = "string"
 
 fun main() {
 
+    //variables constants
     val immutableValue = 10
     var mutableVal = 9
     mutableVal = 20
+
+    // range operator , downTo
+    if (immutableValue in 1..mutableVal + 1) {
+        println("Is in range")
+    }
 
     val valWithDecimalPoint = 3.14
 
@@ -26,34 +25,132 @@ fun main() {
     val floatVal = valWithDecimalPoint.toFloat()
     println(floatVal::class.simpleName)
 
-    // conditions and range operator
-    if (immutableValue in 1..mutableVal+1) {
-        println("fits in range")
+    //user input
+    // using Scanner
+    val read = Scanner(System.`in`)
+    println("Enter value for input : ")
+    var getInput = read.nextInt()
+    // using readln() // readLine()
+    println("Enter value for input : ")
+    var input: Int? = readLine()?.toIntOrNull()
+
+    //array
+    var array: Array<Any>
+    array = Array(5, { i -> i })
+
+    //array = arrayOf<Int>(0,1,2,3,4)
+    array.forEach { element -> print("$element ") }
+    println()
+    println(array[0])
+    println(array.get(2))
+    array.set(0, 5)
+    println(array.size)
+    println(array.component1())
+
+    // Control flow
+
+    //if else
+    val num1 = 10
+    val num2 = 20
+    val result = if (num1 > num2) {
+        "$num1 is greater than $num2"
+    } else {
+        "$num1 is smaller than $num2"
+    }
+    println(result)
+
+    //when, can also have range
+    var dayOfWeek = 1
+    when (dayOfWeek) {
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("Other days")
     }
 
-    //loops
+    //for loop
     for (item in 3..9 step 2) {
-        println(item)
+        print("$item ")
     }
+    println()
 
+    // while
+    while (mutableVal > 0) {
+        print( "$mutableVal ")
+        mutableVal--
+    }
+    println()
+
+    //functions
     var name: String?
     name = null
-    //functions
+
     fun printName(name: String?): Unit {
         if (name != null) {
             println(name)
         } else {
-          println("String is null")
+            println("String is null")
         }
     }
     //name = "Meetraj"
     printName(name)
 
+    //variable input
     fun variableInputs(vararg input: Int) {
-        println(input::class.simpleName)
-        input.forEach { item -> println(item) }
-    }
-    variableInputs(1,2,3,4,5)
 
+        println(input::class.simpleName)
+        input.forEach { item -> print("$item ") }
+        println()
+    }
+    variableInputs(1, 2, 3, 4, 5)
+
+    //lambda
+    fun addTwoNumbers(num1: Int, num2: Int, addLambda: (Int, Int) -> Int){
+        println("Sum of two numbers: "+addLambda(num1, num2))
+    }
+    addTwoNumbers(num1, num2) { n1, n2 -> n1 + n2 }
+
+    //null safety
+    val notNullString = "Hello"
+    val nullableString: String? = null
+    println(notNullString.length)
+
+    //safe call
+    println(nullableString?.length)
+
+    //elvis operator
+    //val lengthOfString: Int = if (nullableString?.length != null) nullableString.length else -1
+    var lengthOfString = nullableString?.length ?: -1
+
+    //not null asssertion (!!)
+    //lengthOfString = nullableString!!.length //will give error
+
+    class B {
+        var c = 1
+    }
+    class A {
+        var b: B? = null
+    }
+    var a: A? = A()
+    println(a?.b?.c ?: 0)
+
+    // scoping functions
+    //let
+    var str: String? = "Hi there"
+    str?.let { println(" $it ") }
+    str = null
+    str?.let { println(" $it ") }
+
+    //run
+    str.run { str += str }
+    println(str)
+
+    //also
+    var m = 1
+    println(m.also { it+1 })
 
 }
