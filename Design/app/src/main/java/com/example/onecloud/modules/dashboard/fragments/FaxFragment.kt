6cd35ac6852183486fce70f032f62cac461e5ca7
Meetraj <rathod.meetraj.d@gmail.com>
2023-06-23 10:56:53 +0530

    Add dashboard activity with bottom navigation

---
+++ Design/app/src/main/java/com/example/onecloud/modules/dashboard/fragments/FaxFragment.kt
@@ -0,0 +1,6 @@
+package com.example.onecloud.modules.dashboard.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.design.R
+
+class FaxFragment : Fragment(R.layout.fragment_fax) { }
---
+++ Design/app/src/main/java/com/example/onecloud/modules/dashboard/fragments/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.example.onecloud.modules.dashboard.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.design.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) { }
